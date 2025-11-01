4101cc65ffda2e2552e014e7f9b591d668f8b2ae
Camilo Seguro <82466584+CamiloSeguro@users.noreply.github.com>
2025-11-01 00:45:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ st.markdown('</div>', unsafe_allow_html=True)
 st.markdown("""
 <hr style="opacity:.15; margin-top:2rem; margin-bottom:1rem;">
 <div style="text-align:center; opacity:.7; font-size:13px; width:50%;">
-  Proyecto Final· <b>Universidad EAFIT</b> · por <b>Camilo Seguro Carvajal</b>
+  Proyecto Final· <b>Universidad EAFIT</b> · <b>Camilo Seguro Carvajal</b>
 </div>
 """, unsafe_allow_html=True)
 
